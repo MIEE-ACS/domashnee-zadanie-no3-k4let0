71c07bbf2ffecae60314257284b921e886af4eef
k4let0 <71322240+k4let0@users.noreply.github.com>
2020-10-30 09:18:59 +0300

    попытка №2

--- hometask3/hometask3/MainWindow.xaml.cs
+++ hometask3/hometask3/MainWindow.xaml.cs
@@ -27,80 +27,146 @@ namespace hometask3
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
+            string abcrus = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя";
+            string abceng = "abcdefghijklmnopqrstuvwxyz";
+
             StringBuilder text = new StringBuilder(textUC.Text);
-            StringBuilder key = new StringBuilder (KeyWord.Text);
-            if (text.Length == 0) MessageBox.Show("Введите шифруемый текст");
+            string key = KeyWord.Text;
+            if (text.Length == 0) MessageBox.Show("Введите текст");
             else if (key.Length == 0) MessageBox.Show("Введите ключ");
+            else if (operation.SelectedIndex == -1) MessageBox.Show("Выберите операцию");
             else
             {
-                int n = key.Length;
+                bool Lang = true; // true=rus falce=eng
                 bool IsCorrect = true;
-                bool Lang = true; // true=rus false=eng
+                int n = key.Length;
                 for (int i = 0; i < n; ++i) if (!char.IsLetter(key[i])) IsCorrect = false; // проверка на отсутствие символов в ключе
-                if (IsCorrect == true) // проверка на единственность языка ключа
+                if (IsCorrect == true)
                 {
-                    Lang = ((key[0] <= 'я' && key[0] >= 'а') || (key[0] >= 'А' && key[0] <= 'Я'));
-                    for(int i = 0; i < n; ++i)
+                    key = key.ToLower();
+                    //задаем язык ключа по первому символу
+                    for (int i = 0; i < 33; ++i) if (key[0] == abcrus[i]) Lang = true;
+                    for (int i = 0; i < 26; ++i) if (key[0] == abceng[i]) Lang = false;
+                    // проверка на единственность языка
+                    for (int i = 0; i < n; ++i)
                     {
-                        if (((key[i] <= 'z' && key[i] >= 'a') || (key[i] >= 'A' && key[i] <= 'Z')) && Lang == true) IsCorrect = false;
-                        if (((key[0] <= 'я' && key[0] >= 'а') || (key[0] >= 'А' && key[0] <= 'Я')) && Lang==false) IsCorrect = false;
+                        for (int j = 0; j < 33; j++) if (key[i] == abcrus[j] && Lang == false) IsCorrect = false;
+                        for (int j = 0; j < 26; j++) if (key[i] == abceng[j] && Lang == true) IsCorrect = false;
                     }
-                } 
-                if (IsCorrect == false) MessageBox.Show("Ключ должен содержать только символы кириллицы или только символы латиницы");
-                else
+                }
+
+                if (IsCorrect == true)
                 {
-                    for (int i = 0; i < n; ++i) char.ToLower(key[i]); 
-                    int k = 0; // счетчик букв
+                    int k = 0;
                     for (int i = 0; i < text.Length; ++i)
                     {
                         if (char.IsLetter(text[i]))
                         {
-                            if (Lang == true && ((text[0] <= 'я' && text[0] >= 'а') || (text[0] >= 'А' && text[0] <= 'Я')))
+                            bool IsRus = true;
+                            if (operation.SelectedIndex == 0) // шифровка
                             {
                                 if (char.IsLower(text[i]))
                                 {
-                                    int letter = text[i] - 'а'; //сдвиг текущей буквы от 'а'(рус)
-                                    letter += key[k % n] - 'а'; //сдвиг текущей буквы после шифровки (может быть больше 33)
-                                    letter %= 33; //сдвиг текущей буквы после шифровки
-                                    text[i] = 'а';
-                                    for (int j = 0; j < letter; ++j) ++text[i];
-                                    ++k;
+                                    int letter;
+                                    for (int j = 0; j < 33; ++j) if (text[i] == abcrus[j]) IsRus = true;
+                                    for (int j = 0; j < 26; ++j) if (text[i] == abceng[j]) IsRus = false;
+                                    if (IsRus == true && Lang == true)
+                                    {
+                                        for (letter = 0; abcrus[letter] != text[i]; ++letter) ; // сдвиг текущей буквы от 'а'(рус)
+                                        for (int j = 0; abcrus[j] != key[k % n]; ++j) ++letter; // сдвиг после шифровки (может быть больше 33)
+                                        letter %= 33;
+                                        text[i] = abcrus[letter];
+                                        ++k;
+                                    }
+                                    if (IsRus == false && Lang == false)
+                                    {
+                                        for (letter = 0; abceng[letter] != text[i]; ++letter) ; // сдвиг текущей буквы от 'a'(eng)
+                                        for (int j = 0; abceng[j] != key[k % n]; ++j) ++letter; // сдвиг после шифровки (может быть больше 26)
+                                        letter %= 26;
+                                        text[i] = abceng[letter];
+                                        ++k;
+                                    }
                                 }
                                 else
                                 {
-                                    int letter = text[i] - 'А'; //сдвиг текущей буквы от 'А'(рус)
-                                    letter += key[k % n] - 'а'; //сдвиг текущей буквы после шифровки (может быть больше 33)
-                                    letter %= 33; //сдвиг текущей буквы после шифровки
-                                    text[i] = 'А';
-                                    for (int j = 0; j < letter; ++j) ++text[i];
-                                    ++k;
+                                    text[i] = char.ToLower(text[i]);
+                                    int letter;
+                                    if (IsRus == true && Lang == true)
+                                    {
+                                        for (letter = 0; abcrus[letter] != text[i]; ++letter) ; // сдвиг текущей буквы от 'а'(рус)
+                                        for (int j = 0; abcrus[j] != key[k % n]; ++j) ++letter; // сдвиг после шифровки (может быть больше 33)
+                                        letter %= 33;
+                                        text[i] = abcrus[letter];
+                                        ++k;
+                                    }
+                                    if (IsRus == false && Lang == false)
+                                    {
+                                        for (letter = 0; abceng[letter] != text[i]; ++letter) ; // сдвиг текущей буквы от 'a'(eng)
+                                        for (int j = 0; abceng[j] != key[k % n]; ++j) ++letter; // сдвиг после шифровки (может быть больше 26)
+                                        letter %= 26;
+                                        text[i] = abceng[letter];
+                                        ++k;
+                                    }
+                                    text[i] = char.ToUpper(text[i]);
                                 }
                             }
-                            if (Lang==false && ((text[i] <= 'z' && text[i] >= 'a') || (text[i] >= 'A' && text[i] <= 'Z')))
-                            { 
+                            else // дешифровка
+                            {
                                 if (char.IsLower(text[i]))
                                 {
-                                    int letter = text[i] - 'a'; //сдвиг текущей буквы от 'a'(eng)
-                                    letter += key[k % n] - 'a'; //сдвиг текущей буквы после шифровки (может быть больше 26)
-                                    letter %= 26; //сдвиг текущей буквы после шифровки
-                                    text[i] = 'a';
-                                    for (int j = 0; j < letter; ++j) ++text[i];
-                                    ++k;
+                                    int letter;
+                                    for (int j = 0; j < 33; ++j) if (text[i] == abcrus[j]) IsRus = true;
+                                    for (int j = 0; j < 26; ++j) if (text[i] == abceng[j]) IsRus = false;
+                                    if (IsRus == true && Lang == true)
+                                    {
+                                        for (letter = 0; abcrus[letter] != text[i]; ++letter) ; // сдвиг текущей буквы от 'а'(рус)
+                                        for (int j = 0; abcrus[j] != key[k % n]; ++j) --letter; // сдвиг после шифровки (может быть меньше 0)
+                                        letter += 33;
+                                        letter %= 33;
+                                        text[i] = abcrus[letter];
+                                        ++k;
+                                    }
+                                    if (IsRus == false && Lang == false)
+                                    {
+                                        for (letter = 0; abceng[letter] != text[i]; ++letter) ; // сдвиг текущей буквы от 'a'(eng)
+                                        for (int j = 0; abceng[j] != key[k % n]; ++j) --letter; // сдвиг после шифровки (может быть меньше 0)
+                                        letter += 26;
+                                        letter %= 26;
+                                        text[i] = abceng[letter];
+                                        ++k;
+                                    }
                                 }
                                 else
                                 {
-                                    int letter = text[i] - 'A'; //сдвиг текущей буквы от 'A'(eng)
-                                    letter += key[k % n] - 'a'; //сдвиг текущей буквы после шифровки (может быть больше 26)
-                                    letter %= 26; //сдвиг текущей буквы после шифровки
-                                    text[i] = 'A';
-                                    for (int j = 0; j < letter; ++j) ++text[i];
-                                    ++k;
+                                    text[i] = char.ToLower(text[i]);
+                                    int letter;
+                                    if (IsRus == true && Lang == true)
+                                    {
+                                        for (letter = 0; abcrus[letter] != text[i]; ++letter) ; // сдвиг текущей буквы от 'а'(рус)
+                                        for (int j = 0; abcrus[j] != key[k % n]; ++j) --letter; // сдвиг после шифровки (может быть меньше 0)
+                                        letter += 33;
+                                        letter %= 33;
+                                        text[i] = abcrus[letter];
+                                        ++k;
+                                    }
+                                    if (IsRus == false && Lang == false)
+                                    {
+                                        for (letter = 0; abceng[letter] != text[i]; ++letter) ; // сдвиг текущей буквы от 'a'(eng)
+                                        for (int j = 0; abceng[j] != key[k % n]; ++j) --letter; // сдвиг после шифровки (может быть меньше 0)
+                                        letter += 26;
+                                        letter %= 26;
+                                        text[i] = abceng[letter];
+                                        ++k;
+                                    }
+                                    text[i] = char.ToUpper(text[i]);
                                 }
                             }
                         }
                     }
+
                     textC.Text = text.ToString();
                 }
+                else MessageBox.Show("Ключ должен содержать только символы кириллицы или только символы латиницы");
             }
         }
     }
